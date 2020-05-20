8a2be2b21b1c226271b0a7e0805bcb23c099005d
Mark E. Haase <mehaase@gmail.com>
2020-05-20 13:40:02 -0400

    Fixing RTD build

--- docs/conf.py
+++ docs/conf.py
@@ -6,14 +6,11 @@
 
 # -- Path setup --------------------------------------------------------------
 
-# If extensions (or modules to document with autodoc) are in another directory,
-# add these directories to sys.path here. If the directory is relative to the
-# documentation root, use os.path.abspath to make it absolute, like shown here.
-#
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
+# To support import of the `example` package during RTD builds, we need to add
+# the parent directory to the path.
+import os, sys
 
+sys.path.insert(0, os.path.abspath(".."))
 
 # -- Project information -----------------------------------------------------
 import subprocess
